2d28d5c9ff549cc1f927dd20e909cb6eff1bee80
= <=>
2024-02-07 22:31:13 +0530

    media consumption added

--- src/main/java/com/Local/api/controllers/consumerController.java
+++ src/main/java/com/Local/api/controllers/consumerController.java
@@ -1,8 +1,10 @@
 package com.Local.api.controllers;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +32,7 @@ public class consumerController {
 		return "Im up!!";
 	}
 	
-	@PostMapping("/updateLocation")
+	@PostMapping(path = "/updateLocation" , consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> changeLocation(@RequestBody changeLocation location) {
 		return service.changeLocation(location);
 	}
